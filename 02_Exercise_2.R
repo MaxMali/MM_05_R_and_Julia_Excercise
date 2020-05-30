ddcadbd48357dd869a8cf7149711941a4b1ff0e0
Max Malinowski <ma.malinowski@ostfalia.de>
2020-05-30 21:00:23 +0200

    Add 02_Exercise_2.R
    Starting Exercise 2

---
+++ 02_Exercise_2.R
@@ -0,0 +1,16 @@
+# Title     : Exercise 2
+# Created by: Max Malinowski
+# Created on: 30.05.20
+
+
+# Exercise 2 (R)
+# Analyse the esoph dataset.
+# Can you derive some useful statements from it? Use data() to see all available datasets.
+
+# data()
+# ?esoph -> Overview of this Dataset
+
+# Loading
+data(esoph)
+# Print data
+head(esoph)
