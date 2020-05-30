07073eb50ae8b89f061c922ed3a749416a73e455
Max Malinowski <ma.malinowski@ostfalia.de>
2020-05-31 01:53:33 +0200

    Add Exercise
    just the start

---
+++ 04_Exercise_4.jl
@@ -0,0 +1,26 @@
+module Excercise_4
+
+# Exercise 4 (JULIA)
+# 1. Create two matrices of the same layout and test if addition and subtraction of the matrix works as expected: C = A + B 
+# 2. Now compare matrix multiplication either this way A * B and this way A .* B. Whats the difference?!
+# 3. What about matrix division with "/" or "\"?!
+# 4. Create a 3x3 integer matrix A with useful numbers. Now try A+1, A-1, A*2, A/2.
+# 5. Now multiply a 3x4 matrix with a suitable (4)vector. 
+
+
+# Part 1
+matrix_a = rand(Int, 4,4)
+matrix_b = rand(Int, 4,4)
+
+println(matrix_a)
+println(matrix_b)
+
+# Part 2
+
+# Part 3
+
+# Part 4
+
+# Part 5
+
+end
