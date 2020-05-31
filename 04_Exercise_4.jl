52d12e694c8090d83ec4d95c6673d160c4a560b6
Max Malinowski <ma.malinowski@ostfalia.de>
2020-05-31 03:33:51 +0200

    Update Exercise 4
    90% done

--- 04_Exercise_4.jl
+++ 04_Exercise_4.jl
@@ -9,18 +9,50 @@ module Excercise_4
 
 
 # Part 1
-matrix_a = rand(Int, 4,4)
-matrix_b = rand(Int, 4,4)
-
+matrix_a = rand(0:10, 4,4)
+matrix_b = rand(0:10, 4,4)
+println("Matrix A:")
 println(matrix_a)
+println("Matrix B:")
 println(matrix_b)
+println("A + B:")
+addition = matrix_a + matrix_b
+println(addition)
+println("A - B:")
+subtraction = matrix_a -matrix_b
+println(subtraction)
 
 # Part 2
 
+# Matrix multiplication
+println("A * B")
+multi_1 = matrix_a * matrix_b
+println(multi_1)
+
+# Element-wise multiplication
+println("A .* B:")
+multi_2 = matrix_a .* matrix_b
+println(multi_2)
 # Part 3
 
 # Part 4
 
+matrix_c = [1 2 3; 3 2 1; 2 3 1]
+println("Matrix C:")
+println(matrix_c)
+println("C+1:")
+# println(matrix_c-1)
+println("C*2:")
+println(matrix_c*2)
+println("C/2:")
+println(matrix_c/2)
+
 # Part 5
+
+matrix_d = rand(0:10, 3,4)
+
+println("Matrix D:")
+println(matrix_d)
+
 
 end
